7c6ceff2ef7b667b6f3e98aa0c511fe8177169d0
Bradley Kukuk <58497863+KukukBrad@users.noreply.github.com>
2020-09-09 19:15:57 -0700

    Update mathFunction.java
    Comments

--- assignmentPR/mathFunction.java
+++ assignmentPR/mathFunction.java
@@ -1,66 +1,98 @@
+// Developed by Robel Tegegne, Melina Diamond-Sagias, Bradley Kukuk, and Abdulraham Alamoud
 class mathFunction
 {
-	public static void main(String args[])
-	{
-	  char[] answer = new char [ 100 ];
-	  int c1 = 1;
-	  int n1 = 1;
-	  int d1 = 1;
-	  int c2 = 2;
-	  int n2 = 2;
-	  int d2 = 3;
-    char d3;
+    public static void main(String args[])
+    {
+        // Initializing Variables
+      char[] answer = new char [ 100 ];
+      int c1 = 1;
+      int n1 = 1;
+      int d1 = 1;
+      int c2 = 2;
+      int n2 = 2;
+      int d2 = 3;
+        char d3;
 
+<<<<<<< 66101945f41578d281bfffca48a189e9e4415987
+        // Checking if the function works successfully
+      if( add( c1, n1, d1, c2, n2, d2, answer, 100) )
+      {
+          //Print Statement
+        System.out.println("\nNice");
+      }
+      // if function fails, it will output the error message
+=======
 	  if( add( c1, n1, d1, c2, n2, d2, answer, 100) )
 	  {
 	    System.out.println(4.16666666);
 	  }
+>>>>>>> fixing the print statement
     else
     {
       System.out.println("There is an error!");
     }
-	}
+    }
 
-	public static boolean add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
-	{
-	  int index = 0;
-	  if( index < len )
-	  {
-	    result[0] = (char)c1;
-	    index++;
-	    if( index < len )
-	    {
-	      result[1] = (char)n1;
-	      index++;
-	      if( index < len )
-	      {
-	        result[2] = (char)d1;
-	        index++;
-	        if( index < len )
-	        {
-	          result[3] = (char)c2;
-	          index++;
-	          if( index < len )
-	          {
-	            result[4] = (char)n2;
-	            index++;
-	            if( index < len )
-	            {
-	              result[5] = (char)d2;
-	              index++;
+    //Function Declaration for add()
+    public static boolean add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+    {
+        // sets index for the function
+      int index = 0;
+        // Checks if index is less than the len
+      if( index < len )
+      {
+        result[0] = (char)c1;
+        //Incrementing the index
+        index++;
+        // Checks if index is less than the len
+        if( index < len )
+        {
+          result[1] = (char)n1;
+          //Incrementing the index
+          index++;
+          // Checks if index is less than the len
+          if( index < len )
+          {
+            result[2] = (char)d1;
+            //Incrementing the index
+            index++;
+            // Checks if index is less than the len
+            if( index < len )
+            {
+              result[3] = (char)c2;
+              //Incrementing the index
+              index++;
+              // Checks if index is less than the len
+              if( index < len )
+              {
+                result[4] = (char)n2;
+                //Incrementing the index
+                index++;
+                // Checks if index is less than the len
                 if( index < len )
                 {
+                  result[5] = (char)d2;
+                  //Incrementing the index
+                  index++;
+                  // Checks if index is less than the len
+                if( index < len )
+                {
+                    // Adding final value to the array as specified in the documentaion for the function
                   result[6] = '\0';
                 }
-	            }
-	          }
-	        }
-	      }
-	    }
-	    return true;
-	  }
-	  return false;
-	}
-	//bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+                }
+              }
+            }
+          }
+        }
+        // Wanted return value
+        return true;
+      }
+      // Failing return value
+      return false;
+    }
+    //bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
 }
+
+
