66101945f41578d281bfffca48a189e9e4415987
Melina Diamond-Sagias <mmd378@nau.edu>
2020-09-09 18:30:29 -0700

    adding function file

---
+++ assignmentPR/mathFunction.java
@@ -0,0 +1,66 @@
+class mathFunction
+{
+	public static void main(String args[])
+	{
+	  char[] answer = new char [ 100 ];
+	  int c1 = 1;
+	  int n1 = 1;
+	  int d1 = 1;
+	  int c2 = 2;
+	  int n2 = 2;
+	  int d2 = 3;
+    char d3;
+
+	  if( add( c1, n1, d1, c2, n2, d2, answer, 100) )
+	  {
+	    System.out.println("\nNice");
+	  }
+    else
+    {
+      System.out.println("There is an error!");
+    }
+	}
+
+	public static boolean add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+	{
+	  int index = 0;
+	  if( index < len )
+	  {
+	    result[0] = (char)c1;
+	    index++;
+	    if( index < len )
+	    {
+	      result[1] = (char)n1;
+	      index++;
+	      if( index < len )
+	      {
+	        result[2] = (char)d1;
+	        index++;
+	        if( index < len )
+	        {
+	          result[3] = (char)c2;
+	          index++;
+	          if( index < len )
+	          {
+	            result[4] = (char)n2;
+	            index++;
+	            if( index < len )
+	            {
+	              result[5] = (char)d2;
+	              index++;
+                if( index < len )
+                {
+                  result[6] = '\0';
+                }
+	            }
+	          }
+	        }
+	      }
+	    }
+	    return true;
+	  }
+	  return false;
+	}
+	//bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+
+}
