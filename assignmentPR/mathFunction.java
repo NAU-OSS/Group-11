fff3b55be7914cb40f9120e5126f2066b67ca4ac
RobelDev <63754258+RobelDev@users.noreply.github.com>
2020-09-16 20:21:13 -0700

    implementing a while loop
    adding a while loop could be nice?

--- assignmentPR/mathFunction.java
+++ assignmentPR/mathFunction.java
@@ -24,6 +24,13 @@ class mathFunction
 	public static boolean add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 	{
 	  int index = 0;
+		
+// 		while(index < len ){
+// 			result[index] = (char) c1;
+// 		if
+// 		index++;
+// 		}
+		
 	  if( index < len )
 	  {
 	    result[0] = (char)c1;
